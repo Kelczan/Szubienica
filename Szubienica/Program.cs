f584955406795e09f74e24c2d17ebff43addf02b
Kelczan <Danio0073@gmail.com>
2023-08-05 15:21:51 +0200

    Random passwords

--- Szubienica/Program.cs
+++ Szubienica/Program.cs
@@ -1,9 +1,36 @@
 ﻿
+using Microsoft.Data.Sqlite;
 using System.Globalization;
 
+string RandomPassword()
+{
+    using (var connection = new SqliteConnection("Data Source=password.db"))
+    {   
+        
+        connection.Open();
+        Random rand = new Random();
+        var command = connection.CreateCommand();
+        command.CommandText =
+        @"
+            SELECT Password
+            FROM Passwords
+            WHERE id = $id
+             ";
+        command.Parameters.AddWithValue("$id", rand.Next(1,4));
+        using (var reader = command.ExecuteReader())
+        {
+            while (reader.Read())
+            {
+                var name = reader.GetString(0);
 
+                return name;
+            }
+        }
+        return null;
+    }
+}
 
-HangMan hangMan = new HangMan("Pajac");
+HangMan hangMan = new HangMan(RandomPassword());
 while (!hangMan.IsFinished())
 {
     hangMan.rysowanie();

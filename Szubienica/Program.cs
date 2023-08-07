6d6911aaaae64eabd02139a8a309feb57ac30191
Kelczan <Danio0073@gmail.com>
2023-08-07 19:51:32 +0200

    Dodane kategorie

--- Szubienica/Program.cs
+++ Szubienica/Program.cs
@@ -1,8 +1,36 @@
 ﻿
 using Microsoft.Data.Sqlite;
+using System.Data;
 using System.Globalization;
+using System.Xml.Linq;
 
-string RandomPassword()
+int KatCount(int kat)
+{
+    using (var connection = new SqliteConnection("Data Source=password.db"))
+    {
+
+        connection.Open();
+        Random rand = new Random();
+        var command = connection.CreateCommand();
+        command.CommandText =
+        @"
+            SELECT count (Passwords_id) FROM Passwords
+            WHERE Kat_id = $id
+             ";
+        command.Parameters.AddWithValue("$id", kat);
+        using (var reader = command.ExecuteReader())
+        {
+            while (reader.Read())
+            {
+                string name = reader.GetString(0);
+
+                return int.Parse(name);
+            }
+        }
+        return 0;
+    }
+}
+string RandomPassword(int kat_id, int count)
 {
     using (var connection = new SqliteConnection("Data Source=password.db"))
     {   
@@ -12,25 +40,46 @@ string RandomPassword()
         var command = connection.CreateCommand();
         command.CommandText =
         @"
-            SELECT Password
-            FROM Passwords
-            WHERE id = $id
+            SELECT Password FROM(
+            SELECT row_number() OVER (ORDER BY Passwords_id ASC) AS rownumber, Password FROM Passwords
+            WHERE Kat_id = $kat_id)
+            WHERE rownumber = $row
              ";
-        command.Parameters.AddWithValue("$id", rand.Next(1,4));
+        command.Parameters.AddWithValue("$row", rand.Next(1,count));
+        command.Parameters.AddWithValue("$kat_id", kat_id);
+        
         using (var reader = command.ExecuteReader())
         {
+            
             while (reader.Read())
             {
                 var name = reader.GetString(0);
-
                 return name;
+              
             }
         }
         return null;
     }
 }
 
-HangMan hangMan = new HangMan(RandomPassword());
+int SelectKat()
+{
+    int i = 0;
+    while (i < 1 || i > 4)
+    {
+        Console.WriteLine(@"Wybierz kategorie:/n 
+                        1: Zwierzęta/n
+                        2: Przedmioty/n
+                        3: Kraje/n
+                        4: Wyzwyska/n");
+        int.TryParse(Console.ReadLine(),out i);
+        Console.Clear();
+    }
+    return i;
+}
+
+int kat = SelectKat();
+HangMan hangMan = new HangMan(RandomPassword(kat,KatCount(kat)));
 while (!hangMan.IsFinished())
 {
     hangMan.rysowanie();

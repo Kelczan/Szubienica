7431efc56d074944ea300b566d82b21481ab9a22
Kelczan <Danio0073@gmail.com>
2023-08-07 21:52:13 +0200

    Zaciąganie kategori.

--- Szubienica/Program.cs
+++ Szubienica/Program.cs
@@ -4,6 +4,36 @@ using System.Data;
 using System.Globalization;
 using System.Xml.Linq;
 
+int Kat()
+{
+    using (var connection = new SqliteConnection("Data Source=password.db"))
+    {
+
+        connection.Open();
+        Random rand = new Random();
+        var command = connection.CreateCommand();
+        command.CommandText =
+        @"
+            SELECT * FROM Kategorie
+             ";
+        Console.WriteLine("Wybierz Kategorie");
+        using (var reader = command.ExecuteReader())
+        {
+            int i = 0;
+            while(reader.Read())
+            {   
+                string str = reader.GetString(0) + ": "+reader.GetString(1);
+                Console.WriteLine(str);
+                i++;
+                
+            }
+            return i;
+        }
+       
+    }
+    
+
+}
 int KatCount(int kat)
 {
     using (var connection = new SqliteConnection("Data Source=password.db"))
@@ -30,7 +60,7 @@ int KatCount(int kat)
         return 0;
     }
 }
-string RandomPassword(int kat_id, int count)
+string RandomPassword(int kat_id)
 {
     using (var connection = new SqliteConnection("Data Source=password.db"))
     {   
@@ -41,11 +71,16 @@ string RandomPassword(int kat_id, int count)
         command.CommandText =
         @"
             SELECT Password FROM(
-            SELECT row_number() OVER (ORDER BY Passwords_id ASC) AS rownumber, Password FROM Passwords
-            WHERE Kat_id = $kat_id)
-            WHERE rownumber = $row
+            SELECT row_number() OVER (ORDER BY Passwords_id ASC) AS rownumber, Password FROM Kategorie
+			INNER JOIN Passwords
+			ON Passwords.Kat_id=Kategorie.Kat_id
+			WHERE Kategorie.Kat_id = $kat_id
+			)
+			WHERE rownumber = $row
+ 
+ 
              ";
-        command.Parameters.AddWithValue("$row", rand.Next(1,count));
+        command.Parameters.AddWithValue("$row", rand.Next(1,KatCount(kat_id)));
         command.Parameters.AddWithValue("$kat_id", kat_id);
         
         using (var reader = command.ExecuteReader())
@@ -64,22 +99,21 @@ string RandomPassword(int kat_id, int count)
 
 int SelectKat()
 {
-    int i = 0;
-    while (i < 1 || i > 4)
+    
+    int i=0;
+    int j = 0;
+    while (j < 1 || j > i)
     {
-        Console.WriteLine(@"Wybierz kategorie:/n 
-                        1: Zwierzęta/n
-                        2: Przedmioty/n
-                        3: Kraje/n
-                        4: Wyzwyska/n");
-        int.TryParse(Console.ReadLine(),out i);
+        i = Kat();
+        int.TryParse(Console.ReadLine(),out j);
+        
         Console.Clear();
     }
-    return i;
+    return j;
 }
 
 int kat = SelectKat();
-HangMan hangMan = new HangMan(RandomPassword(kat,KatCount(kat)));
+HangMan hangMan = new HangMan(RandomPassword(kat));
 while (!hangMan.IsFinished())
 {
     hangMan.rysowanie();
